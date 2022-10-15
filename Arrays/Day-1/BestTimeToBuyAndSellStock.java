0cfc1b9fc1048ffe06b3726de0aa5f636ece84ff
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-10-16 00:14:54 +0530

    Add files via upload

--- Arrays/Day-1/BestTimeToBuyAndSellStock.java
+++ Arrays/Day-1/BestTimeToBuyAndSellStock.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mprofit=0;
+        int buy=0;
+        int sell=1;
+        while(sell < prices.length )   //buy stock
+        {
+           if(prices[sell]>prices[buy]){
+               int profit=prices[sell]- prices[buy];
+               mprofit=Math.max(mprofit, profit);
+           } 
+            else{
+                buy=sell;
+            }
+            sell++;
+            
+        }
+        return mprofit;
+      
+    }
+}
