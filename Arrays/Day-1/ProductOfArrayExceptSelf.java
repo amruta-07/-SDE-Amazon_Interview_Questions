eae0f1bd2ce5be68fbe5c9adaf10a009f5f00e95
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-10-17 22:20:07 +0530

    Add files via upload

--- Arrays/Day-1/ProductOfArrayExceptSelf.java
+++ Arrays/Day-1/ProductOfArrayExceptSelf.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int [] output_arr = new int[nums.length];
 
+        output_arr[0] = 1;
+        for(int i=1; i<nums.length; i++){
+            output_arr[i] = output_arr[i-1] * nums[i-1];
+        }
+
+        int rightProduct = 1;
+        for(int i= nums.length-1; i>=0; i--){
+            output_arr[i] = output_arr[i]*rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+        return  output_arr;
+        
+    }
+}
